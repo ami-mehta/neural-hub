a1408bb1c2d41e4a93ce47e1f918e8f7eb139662
David Currie <davidalexandercurrie@gmail.com>
2020-12-08 00:12:16 -0500

    add socket.io

--- server.js
+++ server.js
@@ -10,9 +10,30 @@ http.listen(process.env.PORT || 3000, process.env.IP, () =>
 );
 
 app.use('/', express.static('public'));
-app.use('/posenet', express.static('posenet-neural-network-classification'));
-app.use('/handpose', express.static('handpose-neural-network-classification'));
-app.use('/facemesh', express.static('facemesh-neural-network-classification'));
+app.use(
+  '/posenet-classification',
+  express.static('posenet-neural-network-classification')
+);
+app.use(
+  '/handpose-classification',
+  express.static('handpose-neural-network-classification')
+);
+app.use(
+  '/facemesh-classification',
+  express.static('facemesh-neural-network-classification')
+);
+app.use(
+  '/posenet-regression',
+  express.static('posenet-neural-network-regression')
+);
+app.use(
+  '/handpose-regression',
+  express.static('handpose-neural-network-regression')
+);
+app.use(
+  '/facemesh-regression',
+  express.static('facemesh-neural-network-regression')
+);
 
 const io = require('socket.io')(http, options);
 io.on('connection', socket => {
