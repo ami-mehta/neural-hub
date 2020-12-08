8e65d0b79429d944110c4975d65d31ca17fe284d
David Currie <davidalexandercurrie@gmail.com>
2020-12-08 14:22:39 -0500

    redo send to max

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ http.listen(process.env.PORT || 3000, process.env.IP, () =>
   console.log('listening on *:3000')
 );
 
+let dataToSendToMax = { model1: 0, model2: 0, model3: 0 };
+let startDataSend = true;
+
 app.use('/', express.static('public'));
 app.use(
   '/posenet-classification',
@@ -39,7 +42,19 @@ const io = require('socket.io')(http, options);
 io.on('connection', socket => {
   console.log(socket.id);
   socket.on('disconnect', () => console.log(`${socket} disconnected`));
-  socket.on('posenet', data => socket.broadcast.emit('posenet', data));
-  socket.on('handpose', data => socket.broadcast.emit('handpose', data));
-  socket.on('facemesh', data => socket.broadcast.emit('facemesh', data));
+  socket.on('posenet', data => {
+    dataToSendToMax.model1 = data[0].label;
+  });
+  socket.on('handpose', data => {
+    dataToSendToMax.model2 = data[0].label;
+  });
+  socket.on('facemesh', data => {
+    dataToSendToMax.model3 = data[0].label;
+  });
+  if (startDataSend) {
+    setInterval(() => {
+      socket.broadcast.emit('max', dataToSendToMax);
+    }, 100);
+    startDataSend = false;
+  }
 });
